46b6d492ccfc1b4e7e646b04fc6d5b34ef50f58f
Johannes Scheffold <johannes.scheffold@kumavision.com>
2019-11-15 16:44:13 +0100

    add generateSearchResultTable Function in Javascript

--- JavaScript/Search.js
+++ JavaScript/Search.js
@@ -1,37 +1,50 @@
 function searchFoods(searchString){
-   var myfoods = getFoods()
-   console.log(myfoods)
+   let myfoods = getFoods();
+   console.log(searchString);
+   var result = myfoods.filter (X => X.name == searchString);
+   console.log(result);
+}
+function generateSearchResultTable(foods){
+    let HTMLString;
+    foods.forEach(food => {
+        HTMLString =  "<tr> <th>" + food.picture + "</th> <th>" + food.description + "</th> <th>" + food.description + "</th> </tr>";
+    });
+    HTMLTable = document.getElementById("SearchResultTable");
+    HTMLTable.innerHTML = HTMLString;
 }
 function getFoods(){
-    foods = [{
-        "Paprika": {
+    foods = [
+        {
+            "name": "Paprika", 
             "picture": "paprika.jpg",
             "description": "basdfladsf",
             "glucose": "10",
             "fructose": "20",
             "lactose": "25"
         },
-        "Gurke": {
+        {
+            "name": "Gurke",
             "picture": "gurke.jpg",
             "description": "basdfladsf",
             "glucose": "10",
             "fructose": "20",
             "lactose": "25"
         },
-        "Karotte": {
+        {
+            "name": "Karotte",
             "picture": "Karotte.jpg",
             "description": "basdfladsf",
             "glucose": "10",
             "fructose": "20",
             "lactose": "25"
         },
-        "Tomate": {
+        {
+            "name": "Gurke", 
             "picture": "paprika.jpg",
             "description": "basdfladsf",
             "glucose": "10",
             "fructose": "20",
             "lactose": "25"
-        }
-    }]
+        }]
     return foods;
 }
