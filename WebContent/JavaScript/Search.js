37d3d66cfb6752f9d12538542fa4461074489636
Johannes Scheffold <johannes.scheffold@kumavision.com>
2019-11-16 21:29:12 +0100

    add error handling and alerts in search funktion

--- WebContent/JavaScript/Search.js
+++ WebContent/JavaScript/Search.js
@@ -1,26 +1,35 @@
-function searchFoods() {
-    searchString = document.getElementById('inputSearch').value;
-    
-    let myfoods = getFoods();
-    var result = myfoods.filter(X => X.name == searchString);
-    generateSearchResultTable(result);
 
+function searchFoods() {
+
+    searchString = document.getElementById('inputSearch').value;
+    if (searchString == "") {
+        swal("Kein Ergebnis!", "Geben Sie etwas in die Suche ein!", "error");
+    } else {
+        let myfoods = getFoods();
+        var result = myfoods.filter(X => X.name == searchString);
+        if (result.length < 1){
+            swal("Kein Ergebnis!", "Es konnte kein Datensatz, der mit ihrer Suche übereinstimmt gefunden werden.", "error");
+        } else{
+            generateSearchResultTable(result);
+        }
+    }
 }
 
-function generateSearchResultTable(foods){
+function generateSearchResultTable(foods) {
     let HTMLString;
     HTMLString = "<tr><th>Bild</th> <th>Name</th> <th>Beschreibung</th></tr>"
     foods.forEach(food => {
-        HTMLString = HTMLString +  "<tr> <td>" + food.picture + "</td> <td>" + food.name + "</td> <td>" + food.description + "</td> </tr>";
+        HTMLString = HTMLString + "<tr> <td>" + food.picture + "</td> <td>" + food.name + "</td> <td>" + food.description + "</td> </tr>";
     });
     HTMLTable = document.getElementById("SearchResultTable");
     HTMLTable.innerHTML = HTMLString;
+    HTMLTable.style.visiblity = "visible";
 }
 
-function getFoods(){
+function getFoods() {
     foods = [
         {
-            "name": "Paprika", 
+            "name": "Paprika",
             "picture": "paprika.jpg",
             "description": "basdfladsf",
             "glucose": "10",
@@ -44,7 +53,7 @@ function getFoods(){
             "lactose": "25"
         },
         {
-            "name": "Gurke", 
+            "name": "Gurke",
             "picture": "paprika.jpg",
             "description": "basdfladsf",
             "glucose": "10",
