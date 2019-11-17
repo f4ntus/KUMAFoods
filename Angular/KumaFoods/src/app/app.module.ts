23e17b668aa6913ca38f4a5f8291c7581aa4c020
Johannes Scheffold <johannes.scheffold@kumavision.com>
2019-11-17 17:54:24 +0100

    new Component: navTitlebar

--- Angular/KumaFoods/src/app/app.module.ts
+++ Angular/KumaFoods/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavTitlebarComponent } from './navTitlebar/navTitlebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavTitlebarComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ Angular/KumaFoods/src/app/navTitlebar/navTitlebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navTitlebar',
+  templateUrl: './navTitlebar.component.html',
+  styleUrls: ['./navTitlebar.component.css']
+})
+export class NavTitlebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
