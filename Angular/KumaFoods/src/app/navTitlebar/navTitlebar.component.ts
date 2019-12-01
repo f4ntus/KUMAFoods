78b535e6751a285cafefd7a5fb4ff419ef943c5a
Johannes Scheffold <johannes.scheffold@kumavision.com>
2019-12-01 23:30:42 +0100

    Copy Pictures

--- Angular/KumaFoods/src/app/navTitlebar/navTitlebar.component.ts
+++ Angular/KumaFoods/src/app/navTitlebar/navTitlebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'app-navTitlebar',
   templateUrl: './navTitlebar.component.html',
   styleUrls: ['./navTitlebar.component.css']
