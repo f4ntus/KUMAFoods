61e22678ff17df774de5428190c2c562240e37d9
Johannes Scheffold <johannes.scheffold@kumavision.com>
2019-12-02 01:53:55 +0100

    Edit Appsettings for Developing and Add C# Model Class for Food

---
+++ KumaFoodsDotNet/KumaFoods/Models/Food.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KumaFoods.Models
+{
+    public class Food
+    {
+        public long FoodID { get; set; }
+        public String FoodName { get; set; }
+        public String FoodDescription { get; set; }
+
+    }
+}
