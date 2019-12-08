5891d5fed24fb1e4df599142fcca5ad5249c4b91
Johannes Scheffold <johannes.scheffold@kumavision.com>
2019-12-08 17:58:18 +0100

    init Projekt like the Book, copy the food cs files from the old Projekt

---
+++ KumaFoodsDotNet/ServerApp/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ServerApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts)
+            : base(opts) { }
+        public DbSet<Food> Foods { get; set; }
+    }
+}
---
+++ KumaFoodsDotNet/ServerApp/Models/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Models
+{
+    public class Food
+    {
+
+
+        [Key]
+        public String FoodName { get; set; }
+        public String FoodDescription { get; set; }
+
+     }
+}
