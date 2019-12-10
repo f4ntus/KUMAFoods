c1a83ee3c97e425d09bae6a21e9da0a52614e5bf
Johannes Scheffold <johannes.scheffold@kumavision.com>
2019-12-10 13:21:38 +0100

    implement HTTP get function to get the hole food table

--- KumaFoodsDotNet/ServerApp/Controllers/FoodValuesController.cs
+++ KumaFoodsDotNet/ServerApp/Controllers/FoodValuesController.cs
@@ -5,9 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ServerApp.Models;
 
+
 /* JOSC: This Class is an Controller for the Webservices of foods. 
  * you can get an singel food with the following link:
  * https://localhost:5001/api/foods/Gurke -> you will receive an Gurke Object
+ * 
+ * it is also possible to receive the hole food Data with Link:
+ * https://localhost:5001/api/foods
+ * 
+ * 
  */
 
 namespace ServerApp.Controllers
@@ -25,6 +31,12 @@ namespace ServerApp.Controllers
         public Food GetFood(String name)
         {
             return context.Foods.Find(name);
+        }
+        [HttpGet]
+        public IEnumerable<Food> GetProducts()
+        {
+            IQueryable<Food> query = context.Foods;
+            return query;
         }
     }
 }
